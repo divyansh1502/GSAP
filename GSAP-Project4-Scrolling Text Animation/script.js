072e9e452a0355ba8b1e97f15ebd1846c0089f58
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-10-12 16:14:11 +0530

    BAnner Animation

---
+++ GSAP-Project4-Scrolling Text Animation/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("wheel",function(dets){
+    if(dets.deltaY>0){
+        gsap.to(".marque",{
+        transform:'translateX(-100%)',
+        duration:4,
+        repeat:-1,
+        ease:'none'
+        })
+        gsap.to(".marque img",{
+            rotate:180
+        })
+    } else {
+        gsap.to(".marque",{
+        transform:'translateX(100%)',
+        duration:4,
+        repeat:-1,
+        ease:'none'
+        })
+        gsap.to(".marque img",{
+            rotate:0
+        })
+    }
+})
